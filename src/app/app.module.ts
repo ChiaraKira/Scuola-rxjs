13f7876cd5f9ba34198d5b218f2c84d094eba015
Chiara Dendena <dendenachiara98@gmail.com>
2024-03-04 16:04:51 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { DettaglioComponent } from './dettaglio/dettaglio.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent,
+    DettaglioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/aula.ts
@@ -0,0 +1,7 @@
+export interface Aula{
+    classe:number;
+    sezione:string;
+    isLaboratorio:boolean;
+    rappresentante: string;
+    mascotteImg:string;
+}
